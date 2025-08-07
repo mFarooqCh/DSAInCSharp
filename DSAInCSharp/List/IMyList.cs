4ee5f01f94a0ac9f03b770f4a8af7204d383496b
Muhammad Farooq Chaudhry <farooqchaudhry749@outlook.com>
2025-08-07 11:08:01 +0500

    array list started

---
+++ DSAInCSharp/List/IMyList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAInCSharp.List
+{
+    public interface IMyList
+    {
+        public void Add(int number);
+        /// <summary>
+        /// Returns the first occurrence of the current number in the list.
+        /// </summary>
+        /// <returns></returns>
+        public int Get();
+        /// <summary>
+        /// Remove the element at current index and sets the current index to the previous element.
+        /// </summary>
+        public void Remove();
+        public bool Find(int number);
+        /// <summary>
+        /// Updates the first occurrence of the specified number in the list. 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public bool Update(int number);
+        /// <summary>
+        /// Returns the number of elements in the list.
+        /// </summary>
+        /// <returns></returns>
+        public int Size();
+        /// <summary>
+        /// Sets the value of current = current - 1
+        /// </summary>
+        public void Back();
+        /// <summary>
+        /// Sets the current index = 1
+        /// </summary>
+        public void Start();
+        /// <summary>
+        /// Sets the current = size 
+        /// </summary>
+        public void Stop();
+    }
+}
---
+++ DSAInCSharp/List/MyArrayList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAInCSharp.List;
+
+public sealed class MyArrayList : IMyList
+{
+    private int[] _array;
+    private int _current = 1;
+    private int _size = 0;
+
+    public MyArrayList(int capacity = 100)
+    {
+        _array = new int[capacity + 1];
+        _size = capacity;
+    }
+
+    public void Add(int number)
+    {
+        _array[_current] = number;
+        _current++;
+    }
+
+    public int Get()
+    {
+        return _array[_current];
+    }
+
+    public void Remove()
+    {
+        _array[_current] = 0;
+        _current--;
+    }
+
+    public bool Find(int number)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(int number)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Size()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Back()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
