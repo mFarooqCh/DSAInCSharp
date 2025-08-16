c4b4f3cc6370eb056b54d0d229ddf580d0c69bb3
Muhammad Farooq Chaudhry <farooqchaudhry749@outlook.com>
2025-08-16 18:04:43 +0500

    test addde

---
+++ TestProjectForDSA/ListTests/ArrayListTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectForDSA.ListTests
+{
+    public sealed class ArrayListTests
+    {
+        [Fact]
+        public void ShouldAddAnInt1()
+        {
+            var list = new DSAInCSharp.List.MyArrayList<int>(10);
+            list.Add(100);
+            list.Add(210);
+
+            Assert.False(list.Contains(123));
+        }
+        
+        [Fact]
+        public void ShouldAddAnInt2()
+        {
+            var list = new DSAInCSharp.List.MyArrayList<int>(10);
+            list.Add(100);
+            list.Add(210);
+
+            Assert.True(list.Contains(100));
+        }
+    }
+}
